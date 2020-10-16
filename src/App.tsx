ac2a98e87526e25d1a473bb506a018a3c95a0fc8
jeanbeanie <msjeanneramos@gmail.com>
2020-10-16 22:11:13 +0000

    updated link styles and added publish date comp

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ import styled from 'styled-components';
 // TODO improve these styled component names haha
 // move colors and sizes to a utility file
 //
+const StyledPublishedDate = styled.h4`
+  margin: 0px;
+  font-size: small;
+  opacity: .7;
+  font-variant: petite-caps;
+`;
 const StyledSideNav = styled.ul`
   list-style: none;
   li { 
@@ -44,12 +50,20 @@ const StyledPage = styled.div`
   p {
     margin-bottom: 3rem;
   }
+  a{
+    text-decoration: none;
+    color: inherit;
+    &:hover {
+      color: white;
+      font-weight: bold;
+    }
+  }
 `;
 
 const StyledColumn = styled.div`
   float: left;
   min-height: 100vh;
-  padding: 1rem;
+  padding: 1rem 0 0 1rem;
   &#left{
     width: 22%;
   }
@@ -81,6 +95,9 @@ const StyledSocialLinks = styled.ul`
     border-radius: 20%;
     background-color: white;
     opacity: 0.7;
+    &:hover{
+      background-color: burlywood;
+    }
   }
   li#twitter-icon{
     background-image: url(https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Tumblr5_svg-256.png)
@@ -119,36 +136,33 @@ function App() {
         <header id="header">
           <StyledAvatar/ >
           <TitleAndSocialList>
-          <StyledSiteTitle> Jeane Ramos </StyledSiteTitle>
+            <StyledSiteTitle><a href="#"> Jeane Ramos </a></StyledSiteTitle>
           <StyledSubTitle> Thinker - Engineer - Artist </StyledSubTitle> 
           <StyledSocialLinks>
-            <li id="twitter-icon"> 
               <a
                 className="App-link"
                 href="https://reactjs.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
+                  <li id="twitter-icon"/> 
               </a>
-            </li>
-            <li id="github-icon"> 
               <a
                 className="App-link"
                 href="https://reactjs.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
+                  <li id="github-icon"/> 
               </a>
-            </li>
-            <li id="linkedin-icon"> 
               <a
                 className="App-link"
                 href="https://reactjs.org"
                 target="_blank"
                 rel="noopener noreferrer"
               >
+                  <li id="linkedin-icon"/> 
               </a>
-            </li>
               </StyledSocialLinks>
             </TitleAndSocialList>
 
@@ -157,20 +171,26 @@ function App() {
           <div>
             <StyledColumn id="left">
               <StyledSideNav>
-                <li>Ramblings</li>
-                <li>Projects</li>
-                <li>Resume</li>
-                <li>Contact Me</li>
+                <a href="#"><li>Ramblings</li></a>
+                 <a href="#"><li>Projects</li></a>
+
+                 <a href="#"><li>Resume</li></a>
+
+                 <a href="#"><li>Contact Me</li></a>
+
               </StyledSideNav>
             </StyledColumn>
               <StyledColumn id="right">
-                <h3>>> What is Lorem Ipsum?</h3>
+                <h3><a href="#">>> What is Lorem Ipsum?</a></h3>
+                  <StyledPublishedDate>Published Oct 10th 2020</StyledPublishedDate>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
-                  <h3>>> What is Lorem Ipsum?</h3>
+                  <h3><a href="#">>> What is Lorem Ipsum?</a></h3>
+                  <StyledPublishedDate>Published Oct 10th 2020</StyledPublishedDate>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
-                  <h3>>> What is Lorem Ipsum?</h3>
+                  <h3><a href="#">>> What is Lorem Ipsum?</a></h3>
+                  <StyledPublishedDate>Published Oct 10th 2020</StyledPublishedDate>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                 </p>
               </StyledColumn>
